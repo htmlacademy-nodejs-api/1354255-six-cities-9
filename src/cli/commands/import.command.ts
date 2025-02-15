9e5c04acd6235cf5248aa78af8c66d270535b20c
Mars <ilon.mars@yandex.ru>
2025-02-16 00:06:35 +0300

    feature: update import command

--- src/cli/commands/import.command.ts
+++ src/cli/commands/import.command.ts
@@ -1,29 +1,35 @@
-import { TSVFileReader } from '../../shared/libs/file-reader/index.js';
+import { getErrorMessage } from '../../shared/helpers/index.js';
+import { FileReaderEvent, TSVFileReader } from '../../shared/libs/file-reader/index.js';
+import { Offer } from '../../shared/types/index.js';
 import { Command } from '../consts.js';
 import { Log } from '../helpers/log.helper.js';
 import { Command as CommandInterface } from './command.interface.js';
 
 export class ImportCommand implements CommandInterface {
+  private onImportedOffer(offer: Offer): void {
+    Log.data(offer);
+  }
+
+  private onCompleteImport(count: number) {
+    Log.info(`${count} rows imported.`);
+  }
+
   public getName(): string {
     return Command.IMPORT;
   }
 
-  public execute(...parameters: string[]): void {
+  public async execute(...parameters: string[]): Promise<void> {
     const [filename] = parameters;
     const fileReader = new TSVFileReader(filename.trim());
 
+    fileReader.on(FileReaderEvent.LINE, this.onImportedOffer);
+    fileReader.on(FileReaderEvent.END, this.onCompleteImport);
+
     try {
       fileReader.read();
-
-      Log.data(fileReader.toArray());
     } catch (err) {
       Log.error(`Can't import data from file: ${filename}`);
-
-      if (!(err instanceof Error)) {
-        throw err;
-      }
-
-      Log.error(`Details: ${err.message}`);
+      Log.error(getErrorMessage(err));
     }
   }
 }
